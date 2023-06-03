bbd46a76c9b65782085b15eeba39d527eb114537
Mieszko Pietrzak <pietrzakmieszko@gmail.com>
2023-06-03 12:00:25 +0200

    token jwt reg exp

--- src/hooks/useLoginStatus.js
+++ src/hooks/useLoginStatus.js
@@ -1,23 +1,17 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+
+const RE_JWT = /^(?:[\w-]*\.){2}[\w-]*$/
 
 const useLoginStatus = () => {
   const [status, setStatus] = useState(false);
-
   useEffect(() => {
     const getLoginStatus = () => {
-        axios
-        .get("https://fcfootball.azurewebsites.net/api/v1/followed-leagues",{
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        })
-        .then((res) => setStatus(true))
-        .catch((err) => setStatus(false));
+      if (localStorage.getItem("token").match(RE_JWT)) setStatus(true);
+      else setStatus(false);
     };
     getLoginStatus();
+    console.log(status)
   });
-
   return status;
 };
 
